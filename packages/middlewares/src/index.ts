64fb2dd7f661967333aff9c905a336b10a8c56e0
vabatta <2137077+vabatta@users.noreply.github.com>
2026-01-24 00:01:14 +0100

    feat(middlewares,qfetch): add middleware-response-error to aggregate packages (#71)

--- packages/middlewares/src/index.ts
+++ packages/middlewares/src/index.ts
@@ -10,12 +10,14 @@
  *   withBaseUrl,
  *   withHeaders,
  *   withQueryParams,
+ *   withResponseError,
  *   withRetryAfter,
  *   withRetryStatus,
  * } from "@qfetch/middlewares";
  * import { compose } from "@qfetch/core";
  *
  * const qfetch = compose(
+ *   withResponseError(),
  *   withRetryStatus({ statuses: [500, 502, 503] }),
  *   withRetryAfter(),
  *   withHeaders({ "Content-Type": "application/json" }),
@@ -28,5 +30,6 @@ export * from "@qfetch/middleware-authorization";
 export * from "@qfetch/middleware-base-url";
 export * from "@qfetch/middleware-headers";
 export * from "@qfetch/middleware-query-params";
+export * from "@qfetch/middleware-response-error";
 export * from "@qfetch/middleware-retry-after";
 export * from "@qfetch/middleware-retry-status";
